1af719e414f9ef3f36d142d9eac5e7c273dbc647
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2019-10-16 23:32:46 -0400

    initial commit of start button, including disappear function, and placeholders

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,48 @@
+// Questions   
+
+
+
+// create header
+
+// create "start quiz" button
+
+// make spot and text for Timer set to 0 default in Nav
+
+// "View High Scores" Link/button in Nav
+
+// questions div
+
+// answers div
+
+// final score div
+
+// initials input
+
+// "Start Quiz" button starts quiz by disappearing 
+
+var startButton = document.querySelector("#start-quiz");
+
+startButton.addEventListener("click", function(){
+    startButton.style.display= "none";
+    
+
+})
+
+
+// and loading what's next >>
+
+// upon "start" button, a question and multiple choice options appear
+
+// set choices to correct or incorrect
+
+// timer running function
+
+// score and initials local storage
+
+
+
+
+
+
+
+
